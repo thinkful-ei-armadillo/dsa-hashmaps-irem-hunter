63090248e58056de2bfea76b44c01930da7c46b1
Hunter Kreshock <hkreshock@gmail.com>
2019-04-08 09:29:23 -0700

    drills 1, 2

--- HashMap.js
+++ HashMap.js
@@ -1,33 +1,11 @@
 'use strict';
 
 class HashMap {
-  constructor(initial_capacity=8){
+  constructor(initialCapacity = 8) {
     this.length = 0;
     this._hashTable = [];
-    this.capacity = initial_capacity;
-    this.deleted = 0;
-  }
-
-  static _hashString(string){
-    let hash = 5381;
-    for (let i = 0; i < string.length; i++) {
-      hash = (hash << 5) + hash + string.charCodeAt(i);
-      hash = hash & hash;
-    }
-    return hash >>> 0;
-  }
-
-  _findSlot(key) {
-    const hash = HashMap._hashString(key);
-    const start = hash % this._capacity;
-
-    for (let i=start; i<start + this._capacity; i++) {
-      const index = i % this._capacity;
-      const slot = this._hashTable[index];
-      if (slot === undefined || (slot.key === key && !slot.DELETED)) {
-        return index;
-      }
-    }
+    this._capacity = initialCapacity;
+    this._deleted = 0;
   }
 
   get(key) {
@@ -36,6 +14,48 @@ class HashMap {
       throw new Error('Key error');
     }
     return this._hashTable[index].value;
+  }
+
+  set(key, value) {
+    const loadRatio = (this.length + this._deleted + 1) / this._capacity;
+    if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+      this._resize(this._capacity * HashMap.SIZE_RATIO);
+    }
+    //Find the slot where this key should be in
+    const index = this._findSlot(key);
+
+    if (!this._hashTable[index]) {
+      this.length++;
+    }
+    this._hashTable[index] = {
+      key,
+      value,
+      DELETED: false
+    };
+  }
+
+  delete(key) {
+    const index = this._findSlot(key);
+    const slot = this._hashTable[index];
+    if (slot === undefined) {
+      throw new Error('Key error');
+    }
+    slot.DELETED = true;
+    this.length--;
+    this._deleted++;
+  }
+
+  _findSlot(key) {
+    const hash = HashMap._hashString(key);
+    const start = hash % this._capacity;
+
+    for (let i = start; i < start + this._capacity; i++) {
+      const index = i % this._capacity;
+      const slot = this._hashTable[index];
+      if (slot === undefined || (slot.key === key && !slot.DELETED)) {
+        return index;
+      }
+    }
   }
 
   _resize(size) {
@@ -53,34 +73,20 @@ class HashMap {
     }
   }
 
-  set(key, value){
-    const loadRatio = (this.length + this._deleted + 1) / this._capacity;
-    if (loadRatio > HashMap.MAX_LOAD_RATIO) {
-      this._resize(this._capacity * HashMap.SIZE_RATIO);
+  static _hashString(string) {
+    let hash = 5381;
+    for (let i = 0; i < string.length; i++) {
+      //Bitwise left shift with 5 0s - this would be similar to
+      //hash*31, 31 being the decent prime number
+      //but bit shifting is a faster way to do this
+      //tradeoff is understandability
+      hash = (hash << 5) + hash + string.charCodeAt(i);
+      //converting hash to a 32 bit integer
+      hash = hash & hash;
     }
-    
-    const index = this._findSlot(key);
-
-    if(!this._hashTable[index]){
-      this.length++;
-    }
-    this._hashTable[index] = {
-      key,
-      value,
-      DELETED: false
-    }; 
-  }
-
-  delete(key) {
-    const index = this._findSlot(key);
-    const slot = this._hashTable[index];
-    if (slot === undefined) {
-      throw new Error('Key error');
-    }
-    slot.DELETED = true;
-    this.length--;
-    this._deleted++;
+    //making sure has is unsigned - meaning non-negtive number.
+    return hash >>> 0;
   }
 }
 
-module.exports = HashMap; 
+module.exports = HashMap;
--- drills.js
+++ drills.js
@@ -1,39 +1,35 @@
 'use strict';
 
-const HashMap = require ('./HashMap');
+const HashMap = require('./HashMap');
 
-
-
-function main(){
+function main() {
   const lor = new HashMap();
-  lor.MAX_LOAD_RATIO = 0.5; 
-  lor.SIZE_RATIO = 3;
+  HashMap.MAX_LOAD_RATIO = 0.5;
+  HashMap.SIZE_RATIO = 3;
 
-  lor.set('Hobbit', 'Bilbo');
-  lor.set('Hobbit', 'Frodos');
-//   lor.set('Wizard', 'Gandalf');
-//   lor.set('Human', 'Aragon');
-//   lor.set('Elf', 'Legolas');
-//   lor.set('Maiar', 'Necromancer');
-//   lor.set('Maiar', 'Sauron');
-//   lor.set('RingBearer', 'Gollum');
-//   lor.set('LadyOfLight', 'Galadriel');
-//   lor.set('HalfElven', 'Arwen');
-//   lor.set('Ent', 'Treebeard');
-  
-  display(lor);
-//   console.log(JSON.stringify(lor, null, 2));   
+  lor.set('Hobbit', 'Bilbo'),
+  lor.set('Hobbit', 'Frodos'),
+  lor.set('Wizard', 'Gandalf'),
+  lor.set('Human', 'Aragon'),
+  lor.set('Elf', 'Legolas'),
+  lor.set('Maiar', 'Necromancer'),
+  lor.set('Maiar', 'Sauron'),
+  lor.set('RingBearer', 'Gollum'),
+  lor.set('LadyOfLight', 'Galadriel'),
+  lor.set('HalfElven', 'Arwen'),
+  lor.set('Ent', 'Treebeard');
+  // return lor.get('Hobbit');
+  return display(lor);
 }
 
-function display(data) {
-  let result =[];
-  console.log('data', data); 
-    console.log('hashtable', data._hashTable);
+// console.log(main());
 
-  for(let i=0; i<data._hashTable.length; i++){
-    result.push(data._hashTable.charAt(i));
+function display(data) {
+  let result = [];
+  console.log(data);
+
+  for (let i = 0; i < data._hashTable.length; i++) {
+    result.push(data._hashTable[i]);
   }
   return result;
 }
-
-console.log(main());
