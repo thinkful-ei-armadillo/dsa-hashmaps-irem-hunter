1ac66ab7f8b098a10b764052f4e9e21319f14702
Hunter Kreshock <hkreshock@gmail.com>
2019-04-08 13:18:01 -0700

    drills 5, 6

--- drills.js
+++ drills.js
@@ -34,15 +34,77 @@ function display(data) {
   return result;
 }
 
-function removeDuplicates(str){
+function removeDuplicates(str) {
   let alp = new Map();
 
-  for(let i =0 ; i< str.length; i++){
-    if (!alp.has(str[i])){
+  for (let i = 0; i < str.length; i++) {
+    if (!alp.has(str[i])) {
       alp.set(str[i], str[i]);
-    } 
+    }
   }
   console.log(alp.keys());
 }
 
-console.log(removeDuplicates('google all that you think can think of'));
+// console.log(removeDuplicates('google all that you think can think of'));
+
+function palindrome(str) {
+  let words = new HashMap();
+  let doubles = null;
+  let numOfDoubles = 0;
+  if (str.length % 2 === 0) {
+    doubles = str.length / 2;
+  } else {
+    doubles = Math.floor(str.length / 2);
+  }
+  for (let i = 0; i < str.length; i++) {
+    try {
+      words.get(str[i]);
+    } catch (e) {
+      words.set(i, str[i]);
+    }
+  }
+  for (let count = 0; count < str.length; count++) {
+    for (let i = 0; i < str.length; i++) {
+      if (count !== i) {
+        if (words.get(count) === words.get(i)) {
+          numOfDoubles++;
+        }
+      }
+    }
+  }
+  if (doubles === numOfDoubles / 2) {
+    return true;
+  }
+  return false;
+}
+
+// console.log(palindrome('racecar'));
+
+function group(arr) {
+  let arrays = new HashMap();
+  let newArr = [];
+  let finalArr = [];
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i].split('').sort());
+    arrays.set(i, newArr[i].join(''));
+  }
+  for (let count = 0; count < arr.length; count++) {
+    if (total < arr.length) {
+      let tempArr = [];
+      for (let i = 0; i < arr.length; i++) {
+        if (arrays.get(count) === arrays.get(i)) {
+          tempArr.push(arr[i]);
+          total++;
+        }
+      }
+
+      finalArr.push(tempArr);
+    } else {
+      break;
+    }
+  }
+  return finalArr;
+}
+
+// console.log(group(['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race']));
